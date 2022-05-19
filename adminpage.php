0e3e2a4aea89c6e07d35568c794309d9766063ae
Tsukimiyu <81209014+Tsukimiyu@users.noreply.github.com>
2022-05-20 05:18:01 +0800

    Update adminpage.php
    added initial status of current inventory in admin page. may add a pie-chart if requested

--- adminpage.php
+++ adminpage.php
@@ -16,8 +16,18 @@
 
 
     }
-    
 
+    p {
+      margin-top: 10px;
+      margin-left: 1%;
+      font-weight: bold;
+      font-size: 2em;
+    }
+    
+    table {
+      margin: 5px;
+      width: 50%;
+    }
 
 	</style>
 	<title>Admin Navigation Bar</title>
@@ -41,5 +51,42 @@
       </ul>
     </div>
   </nav>
+    <p> Current Inventory:</p>
+  <?php $conn = mysqli_connect('localhost', 'gypsy', 'admin', 'database');
+$sql = "SELECT ID, name, Qty FROM inventory";
+$result = $conn->query($sql);
+
+echo "<font color=Black> <table border='1'>
+
+<tr>
+
+<th>ID</th>
+
+<th>Name</th>
+
+<th>Qty</th>
+
+</tr>";
+
+while($row = $result->fetch_assoc())
+  {
+
+  echo "<tr>";
+
+  echo "<td>" . $row['ID'] . "</td>";
+
+  echo "<td>" . $row['name'] . "</td>";
+
+  echo "<td>" . $row['Qty'] . "</td>";
+
+  echo "</tr>";
+
+  }
+
+echo "</table>";
+
+?>
+
+
 </body>
 </html>
