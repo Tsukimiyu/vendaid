638c910cf1da1d5ec1504f674897fdee8bb28bb8
Tsukimiyu <81209014+Tsukimiyu@users.noreply.github.com>
2022-05-22 23:26:20 +0800

    Update RFID.php
    //private RFID dimensions w/ back button

--- RFID.php
+++ RFID.php
@@ -30,6 +30,7 @@ if (isset($_POST['RFID'])){
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="css/style.css">
   <!-- Compiled and minified CSS -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
 
@@ -55,7 +56,6 @@ if (isset($_POST['RFID'])){
 <style>body {
     margin: 0;
     padding: 0;
-    font-family: sans-serif;
     background: url() no-repeat;
     background-size: cover;
     background-color: white;
@@ -81,17 +81,58 @@ body {
     color: white;
 }
   
-}</style>
+.button {
+  margin-top:20px;
+  height: 15%;
+  width: 15%;
+  margin-left: 2%;
+  border-radius: 20px;
+  border: 1px solid #74ABFF;
+  background-color: #74ABFF;
+  color: #FFFFFF;
+  font-size: 12px;
+  font-weight: bold;
+  padding: 1em;
+  letter-spacing: 1px;
+  text-transform: uppercase;
+  transition: transform 80ms ease-in;
+}
+
+.button:active {
+  transform: scale(0.95);
+}
+
+.button:focus {
+  background-color: #5C9DFF;
+  outline: none;
+}
+
+.textbox {
+  height: 2%;
+}
+
+.rfidtxt {
+  display: none;
+}
+
+.title1{
+
+  margin: 5px;
+  padding: 5px;
+ display: inline-block;
+
+}
+</style>
 <body>
-  <center> <h1 style= "font-family: sans; color:black;">Please tap ID on scanner</h1> </center>
-  <div class="row"></div>
-  <div class="row"></div>
-  <center><img src="images/RFID1.png"
-      width="450"
-      height="450"></center>
-  <form name="form" action="" method="post">
-  <div class="textbox"><input style=back type="text" autofocus="autofocus" name="RFID" id="RFID">
-  </div>
   
+  <div class="container-1">
+    <a href="carousel.php"><input type="button" placeholder="Back" class="button" value=" < Back"></a> <center> <h2 class="title1" style= "color:black;">Please tap ID on scanner</h2> </center>
+   <center>
+    <div class="row"></div>
+    <img src="images/RFID1.png" width="35%" height="35%"></center>
+  <form name="form" action="" method="post">
+  <div class="textbox"><input style=back type="text" autofocus="autofocus" name="RFID" id="RFID" class="rfidtxt">
+  </div>
+  </div>
 </body>
 </html>
